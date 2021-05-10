9bad44459b30cd024790b45c7897a968ea099757
Malek-MK <57958741+Malek-MK@users.noreply.github.com>
2021-05-10 16:36:56 +0100

    Update ListArbitrators.jsx

--- imports/ui/ListArbitrators.jsx
+++ imports/ui/ListArbitrators.jsx
@@ -12,7 +12,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
 import { Meteor } from 'meteor/meteor';
- 
+ import Modal from "react-bootstrap/Modal";
+
 const notyf = new Notyf({
     duration: 2000,
     position: {
@@ -21,7 +22,41 @@ const notyf = new Notyf({
     }
   })
 const ListArbitrators = ({ arbitrator, fetch}) => {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver()
+  });
+ const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     const [mail,setMail]=useState([])
+    const UpdatePlayer = data => {
+    Meteor.call('updatePlayer', { id: player._id, data }, (err) => {
+      if (err) {
+        console.log('Updated failed')
+        notyf.error("Updated failed")
+      }
+      else {
+        notyf.success("Updated with success")
+        console.log('Updated with success')
+        setShow(false);
+        fetch();
+      }
+    });
+  };
+
+  const Delete = () => {
+    Meteor.call('deletePlayer', player._id, (err) => {
+      if (err) {
+        notyf.error("Deleted failed");
+        console.log('Deleted failed');
+      }
+      else {
+        notyf.success("Deleted with success");
+        console.log('Deleted with success');
+        fetch();
+      }
+    });
+  }
     useEffect(() => {
         setMail(arbitrator.emails)
         fetch(); 
@@ -43,23 +78,52 @@ const ListArbitrators = ({ arbitrator, fetch}) => {
              <i class="fa fa-trash-o fa-lg"></i>  Delete
              </Button>        </td>
          </tr>
-         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Update Arbitrator</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body"> 
-      Show a second modal and hide this one with the button below.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+  <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update this player</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(UpdatePlayer)} id="update">
+            <label>Name :</label>
+            <input
+              type="text"
+              name="name"
+              ref={register}
+              placeholder="Player Name"
+              className="form-control"
+            />
+            <p className="text-danger">{errors.name?.message}</p>
+            <label>Number :</label>
+            <input
+              type="number"
+              name="number"
+              ref={register}
+              placeholder="Number"
+              className="form-control"
+            />
+            <p className="text-danger">{errors.number?.message}</p>
+            <label htmlFor="Role">Role :</label>
+            <input
+              type="text"
+              name="role"
+              ref={register}
+              placeholder="Role"
+              className="form-control"
+            />
+            <p className="text-danger">{errors.role?.message}</p>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-primary" form="update" type="submit">
+            Save Update
+          </button>
+        </Modal.Footer>
+      </Modal>
        </>
     )
 }
